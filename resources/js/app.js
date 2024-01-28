492b28cd7c64b4ed5cfdab2373c4d6cba34dc5e2
Abdallah Menacer <abdallah_0104@outlook.com>
2024-01-28 15:51:55 +0000

    initial commit. Model/controller/migration created for notes and api routes created

---
+++ resources/js/app.js
@@ -0,0 +1,14 @@
+//resources/js/app.js
+require("./bootstrap");
+import { createApp } from "vue";
+
+import Index from "./components/Index.vue";
+
+
+const app = createApp({});
+
+app.component("index", Index);
+
+
+app.mount('#app');
+
